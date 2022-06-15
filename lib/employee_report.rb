221c81142e016552b32cb45e5ecc432b32cc7a7f
Andy Waite <andyw8@users.noreply.github.com>
2022-06-15 11:02:02 -0400

    Done

--- lib/employee_report.rb
+++ lib/employee_report.rb
@@ -1,2 +1,46 @@
+class Employee
+  def initialize(name, age)
+    @name, @age = name, age
+  end
+
+  attr_reader :age
+
+  def name
+    @name.upcase
+  end
+
+  def <=>(other)
+    other.name <=> name
+  end
+end
+
 class EmployeeReport
+  def self.from_hashes(employee_hashes)
+    new(employee_hashes.map do |e|
+      Employee.new(e[:name], e[:age])
+    end)
+  end
+
+  def initialize(employees)
+    @employees = employees
+  end
+
+  def over_18
+    # as_capitalized(employees_by_name).select { |employee| employee[:age] >= 18 }
+    employees_by_name.select { |employee| employee.age >= 18 }
+  end
+
+  # def as_capitalized(employees)
+  #   employees.map do |e|
+  #     e.merge(name: e[:name].upcase)
+  #   end
+  # end
+
+  private
+
+  def employees_by_name
+    employees.sort
+  end
+
+  attr_reader :employees
 end
--- test/employee_report_test.rb
+++ test/employee_report_test.rb
@@ -2,7 +2,43 @@ require "test_helper"
 require "employee_report"
 
 class EmployeeReportTest < ActiveSupport::TestCase
-  test "example" do
-    assert true, "Example passes, tests are executing correctly"
+  test "list all employee who are over 18" do
+    max = Employee.new("Max", 17)
+    sepp = Employee.new("Sepp", 18)
+    nina = Employee.new("Nina", 15)
+    mike = Employee.new("Mike", 51)
+
+    employees = [
+      max, sepp, nina, mike
+    ]
+    employee_report = EmployeeReport.new(employees)
+
+    result = employee_report.over_18
+
+    assert_includes result, sepp
+    assert_includes result, mike
+  end
+
+  test "employee list is ordered reverse alphabetically" do
+    sepp = Employee.new("Sepp", 18)
+    mike = Employee.new("Mike", 51)
+
+    employees = [
+      sepp, mike
+    ]
+    employee_report = EmployeeReport.new(employees)
+
+    result = employee_report.over_18
+
+    assert_equal [sepp, mike], result
+  end
+
+  test "employee list is capitalized" do
+    sepp = Employee.new("Sepp", 18)
+    employee_report = EmployeeReport.new([sepp])
+
+    result = employee_report.over_18
+
+    assert_includes result.sole.name, "SEPP"
   end
 end
